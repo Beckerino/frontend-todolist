f93315f52e548e7e783dbc4ec1921ec69afa0a59
Diego Paiva <paivadiego7797@gmail.com>
2019-12-12 08:57:20 -0400

    Adição vue-resource via yan package

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./quasar";
+import VueResource from "vue-resource";
 
 Vue.config.productionTip = false;
+Vue.use(VueResource);
 
 new Vue({
   router,
