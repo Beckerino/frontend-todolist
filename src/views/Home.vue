21b9f0807f40d26dd9a62d0df2c47ceb8259a3a9
Diego Paiva <paivadiego7797@gmail.com>
2019-12-17 07:59:32 -0400

    Adição de botão menu principal home
    E separa

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,40 @@
   <div class="home">
     <div class="q-pa-md">
       <q-table
-        class="my-sticky-virtscroll-table"
-        virtual-scroll
-        table-style="max-height: 400px"
-        :rows-per-page-options="[0]"
-        :virtual-scroll-sticky-start="48"
+        :filter="filter"
         row-key="id"
+        no-data-label="não há nada na nuvem"
+        no-results-label="o filtro não encontrou resultados"
         title="Atividades"
         :data="Atividades"
         :columns="columns"
-      />
+        selection="single"
+        :selected.sync="selected"
+      >
+        <template v-slot:top-right="props">
+          <q-input
+            borderless
+            dense
+            debounce="300"
+            v-model="filter"
+            placeholder="Procurar"
+          >
+            <q-icon slot="append" name="search" />
+          </q-input>
+        </template>
+
+        <template v-slot:no-data="{ icon, message, filter }">
+          <div class="full-width row flex-center text-accent q-gutter-sm">
+            <q-icon size="2em" name="sentiment_dissatisfied" />
+            <span> Poxa... não foi encontrado nada {{ message }} </span>
+            <q-icon size="2em" :name="filter ? 'filter_b_and_w' : icon" />
+          </div>
+        </template>
+      </q-table>
+      <div v-if="selecao()" class="row justify-center">
+        <q-btn label="Alterar Item" v-on:click="alterar()" />
+        <q-btn label="Deletar Item" v-on:click="deletar()" color="red" />
+      </div>
     </div>
   </div>
 </template>
@@ -23,15 +47,20 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   computed: {
     ...mapActions({
-      ActAtividades: "act_atividades"
+      ActAtividades: "act_get_atividade"
     }),
     ...mapGetters({
       Atividades: "get_atividades"
     })
   },
+  created() {
+    this.ActAtividades();
+  },
   name: "home",
   data() {
     return {
+      filter: "",
+      selected: [],
       columns: [
         {
           name: "name",
@@ -64,6 +93,24 @@ export default {
         }
       ]
     };
+  },
+  beforeDestroy() {
+    clearInterval(this.interval1);
+  },
+  methods: {
+    selecao() {
+      if (this.selected.length >= 1) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    alterar() {
+      this.$router.push("/Alterar");
+    },
+    deletar() {
+      this.$router.push("/Excluir");
+    }
   }
 };
 </script>
